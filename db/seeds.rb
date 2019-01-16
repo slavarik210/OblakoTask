2b7cb4206303398ff6472e2889847b21f5755ccb
skarus <kovrizhin96@mail.ru>
2019-01-16 12:47:04 +0300

    add db from seeds.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+seed_file = Rails.root.join('db', 'seeds', 'seeds.yml')
+config = HashWithIndifferentAccess.new(YAML::load_file(seed_file))
+
+config['projects'].each do |project|
+  p = Project.create(title: project['title'])
+  project['todos'].each do |todo|
+    p.todos.create(todo)
+  end
+end
